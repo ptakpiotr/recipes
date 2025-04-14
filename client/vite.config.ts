9c8a1255114ff959191568b2449b43552db8bc15
ptakpiotr <piter020@outlook.com>
2025-04-15 00:18:43 +0200

    Add basic PWA config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   server: {
     cors: {
       origin: "*",
